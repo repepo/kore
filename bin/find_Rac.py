d4ce6f3384bcf3a1d419bf9c4ff09d7bb4cb94a3
AnkitBarik <ankit.barik@gmail.com>
2020-08-31 02:34:34 -0400

    Update find_Rac, doesn't work yet

--- bin/find_Rac.py
+++ bin/find_Rac.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 
 import numpy as np
 import os
@@ -6,10 +6,10 @@ import sys
 from mpi4py import MPI
 from scipy.optimize import newton, brentq
 
-opts='-st_type sinvert -eps_error_relative ::ascii_info_detail'
+opts='-st_type sinvert -eps_error_relative'
 
-mmin = 11
-mmax = 13
+mmin = 12
+mmax = 12
 marr = np.arange(mmin,mmax+1)
 
 comm = MPI.COMM_WORLD
@@ -19,18 +19,44 @@ mperproc = int(len(marr)/ncpus)
 
 comm.scatter(marr,root=0)
 
-Ra = 3.5e5
+Ra = 3.4e5
 
-def get_sigma(ncpus, opts):
-    os.system('./submatrices.py %d' %ncpus)
+def get_sigma(Ra,ncpus, opts):
+    Ra = 10**Ra
+    os.system('sed -i "s/RaUsr/%f/" parameters.py' %Ra)
     os.system('mpiexec -n %d ./assemble.py' %ncpus)    
     os.system('mpiexec -n %d ./solve_nopp.py %s' %(ncpus,opts))
     eig = np.loadtxt('eigenvalues.dat')
-    Idx = np.argmax(eig[0,:])
-    sigma_real_c = eig[Idx,:][0]
+    Idx = np.argmax(eig[:,0])
+    sigma_c = eig[Idx,0]
 
-    return sigma_real_c
+    print(sigma_c)
 
+    return sigma_c
+
+# Function copied from SINGE - looks for Ra bounds
+def bracket_brentq(f, x1, x2=None, dx=0.3, tol=1e-6, maxiter=200, args=None):
+    y1 = f(x1, *args)
+    print(y1)
+    dx = abs(dx)         # dx must be positive.
+    if x2 is not None:   # check that we actually have a bracket
+        y2 = get_sigma(x2, *args)
+        if y2*y1 > 0:        # we don't have a bracket !!
+            if (abs(y2) < abs(y1)):
+                x1,y1 = x2,y2        # start from the value closest to the root
+            x2 = None        # search needed.
+    if x2 is None:        # search for a bracket
+        x2 = x1
+        if y1>0:  dx = -dx        # up or down ?
+        while True:
+            x2 += dx
+            y2 = f(x2, *args)
+            if y2*y1 < 0: break
+            print(x2)
+            x1,y1 = x2,y2
+    # Now that we know that the root is between x1 and x2, we use Brent's method:
+    x0 = brentq(f, x1, x2, maxiter=maxiter, xtol=tol, rtol=tol, args=args)
+    return x0
 
 for m in range(mperproc):
     mIdx = rank*mperproc + m
@@ -40,32 +66,8 @@ for m in range(mperproc):
     os.chdir(mdir)
     os.system('cp ../params_conv.py parameters.py')
     os.system('cp ../assemble.py ../utils*.py ../solve_nopp.py ../submatrices.py ../bc_variables.py .')
-    os.system('sed -i "s/mUsr/%d/" parameters.py' %marr[mIdx])
+    os.system('sed -i "s/mUsr/%d/" parameters.py' %marr[mIdx]) 
     os.system('sed -i "s/RaUsr/%f/" parameters.py' %Ra)
-    sigma_real_c = get_sigma(ncpus,opts)
-    
-
-    
-
-
-# Function copied from SINGE - looks for Ra bounds
-def bracket_brentq(f, x1, x2=None, dx=0.3, tol=1e-6, maxiter=200, args=None):
-	y1 = f(x1, *args)
-	dx = abs(dx)		 # dx must be positive.
-	if x2 is not None:   # check that we actually have a bracket
-		y2 = optimize_Ra(x2, *args)
-		if y2*y1 > 0:		# we don't have a bracket !!
-			if (abs(y2) < abs(y1)):
-				x1,y1 = x2,y2		# start from the value closest to the root
-			x2 = None		# search needed.
-	if x2 is None:		# search for a bracket
-		x2 = x1
-		if y1>0:  dx = -dx		# up or down ?
-		while True:
-			x2 += dx
-			y2 = f(x2, *args)
-			if y2*y1 < 0: break
-			x1,y1 = x2,y2
-	# Now that we know that the root is between x1 and x2, we use Brent's method:
-	x0 = brentq(f, x1, x2, maxiter=maxiter, xtol=tol, rtol=tol, args=args)
-	return x0
+    os.system('./submatrices.py %d' %ncpus)
+    Rac = bracket_brentq(get_sigma,np.log10(Ra),args=(ncpus,opts))
+    print(10**Rac)
