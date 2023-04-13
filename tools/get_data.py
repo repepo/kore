02c3fec26ed4916a0607b905612fe13513964460
fleursys <seuren.fleur@gmail.com>
2023-04-13 11:31:37 +0200

    added timescales and thermal energy computation

--- tools/get_data.py
+++ tools/get_data.py
@@ -33,10 +33,10 @@ def load_results(filename):
                                                              "Dohm3", "rmtorq_cmb", "imtorq_cmb"])
         df = pd.concat([df, mag], axis=1)
     if exists(filename + '.tmp'):
-        tmp = pd.read_csv(filename + '.tmp', sep=' ', names=['Dtemp'])
+        tmp = pd.read_csv(filename + '.tmp', sep=' ', names=['Dbuoy', 'TE', 'Dtemp', 'Dadv'])
         df = pd.concat([df, tmp], axis=1)
     if exists(filename + '.cmp'):
-        cmp = pd.read_csv(filename + '.cmp', sep=' ', names=['Dcomp'])
+        cmp = pd.read_csv(filename + '.cmp', sep=' ', names=['Dbuoy_comp', 'CE', 'Dcomp', 'Dadv_comp'])
         df = pd.concat([df, cmp], axis=1)
     if exists(filename + '.eig'):
         err = pd.read_csv(filename + '.eig', sep=' ', names=['resid1', 'resid2'])
@@ -66,10 +66,10 @@ def load_result():
                                                              "Dohm3", "rmtorq", "imtorq"])
         df = pd.concat([df, mag], axis=1)
     if exists('thermal.dat'):
-        tmp = pd.read_csv('thermal.dat', sep=' ', names=['Dtemp'])
+        tmp = pd.read_csv('thermal.dat', sep=' ', names=['Dbuoy', 'TE', 'Dtemp', 'Dadv'])
         df = pd.concat([df, tmp], axis=1)
     if exists('compositional.dat'):
-        cmp = pd.read_csv('compositional.dat', sep=' ', names=['Dcomp'])
+        cmp = pd.read_csv('compositional.dat', sep=' ', names=['Dbuoy_comp', 'CE', 'Dcomp', 'Dadv_comp'])
         df = pd.concat([df, cmp], axis=1)
     if exists('error.dat'):
         err = pd.read_csv('error.dat', sep=' ', names=['resid1', 'resid2'])
@@ -89,14 +89,22 @@ else:
     df = load_result()
 
 # --- COMPUTATION ---
+# scaling factors
+if (df['time_scale'] == 1).all():
+    df['scale_factor'] = 1/df['Ek']
+elif (df['time_scale'] == 2).all():
+    df['scale_factor'] = 1/df['Le']
+else:
+    df['scale_factor'] = np.ones(len(df))
+
 # kinetic energy parameters
 df['KE'] = df['KP'] + df['KT']
 
 df['t2p'] = df['KT']/df['KP']
 df['p2t'] = df['KP']/df['KT']
 
-df['Dkin'] = df['rDkin']*df['Ek']
-df['Dint'] = df['Dint']*df['Ek']
+df['Dkin'] = df['rDkin']*df['Ek']*df['scale_factor']
+df['Dint'] = df['Dint']*df['Ek']*df['scale_factor']
 
 if (df['Dint1'] != 0).all() or (df['Dint2'] != 0).all() or (df['Dint3'] != 0).all():
     df['Dint_bulk1'] = df['Dint'] - df['Dint1'] - df['Dint2']
@@ -114,8 +122,22 @@ if (df['forcing'] == 0).all():
     del df['Af_cmb']
     df['freq'] = df['itau']
     df['damp'] = df['rtau']
+    df['pss'] = np.zeros(len(df))
+    df['pvf'] = np.zeros(len(df))
 else:
     df['freq'] = df['wf']
+    if (df['forcing'] == 1).all():
+        df['pss'] = np.zeros(len(df))
+        df['pvf'] = df['rpow']
+    elif (df['forcing'] == 7).all():
+        df['pvf'] = 0
+        df['pss'] = df['rpow']
+    elif (df['forcing'] == 8).all():
+        df['pss'] = np.zeros(len(df))
+        df['pvf'] = df['rpow']
+    elif (df['forcing'] == 9).all():
+        df['pvf'] = np.zeros(len(df))
+        df['pss'] = df['rpow']
 
 del df['wf']
 del df['rtau']
@@ -125,6 +147,8 @@ del df['itau']
 if (df['magnetic'] == 0).all():
     del df['Em']
     del df['Le2']
+    del df['mu']
+    del df['B0_norm']
     df['Dohm'] = np.zeros(len(df))
 else:
     df['Le'] = np.sqrt(df['Le2'])
@@ -133,7 +157,7 @@ else:
 
     df['ME'] = df['MP'] + df['MT']
     df['mtorq'] = df['rmtorq'] + 1j*df['imtorq']
-    df['Dohm'] = (df['DohmP'] + df['DohmT'])*((1-df['tA'])*df['Le2']*df['Em'] + df['tA']*df['Le']*df['Em'])
+    df['Dohm'] = (df['DohmP'] + df['DohmT'])*df['Le2']*df['Em']*df['scale_factor']
 
     df['o2v'] = df['Dohm']/df['Dint']
 
@@ -150,25 +174,30 @@ else:
     del df['Dohm2']
     del df['Dohm3']
 
-# thermal and compositional parameters
+# thermal parameters
 if (df['thermal'] == 0).all():
     del df['Prandtl']
     del df['Ra']
+    del df['rc']
+    del df['h']
     df['Dtemp'] = np.zeros(len(df))
-    if (df['compositional'] == 0).all():
-        del df['rc']
-        del df['h']
 else:
     df['Brunt'] = scimath.sqrt(-df['Ra']/df['Prandtl'])*df['Ek']
+    df['Dbuoy'] = df['Dbuoy']*df['scale_factor']*(df['Ek']**2)*df['Ra']/df['Prandtl']
+    df['Dtemp'] = df['Dtemp']*df['scale_factor']*df['Ek']/df['Prandtl']
+
+# compositional parameters
 if (df['compositional'] == 0).all():
     del df['Schmidt']
     del df['Ra_comp']
-    df['Dcomp'] = np.zeros(len(df))
+    df['Dbuoy_comp'] = np.zeros(len(df))
 else:
-    df['Brunt_comp'] = scimath.sqrt(-df['Ra_comp']/df['Prandtl'])*df['Ek']
+    df['Brunt_comp'] = scimath.sqrt(-df['Ra_comp']/df['Schmidt'])*df['Ek']
+    df['Dbuoy_comp'] = df['Dbuoy_comp']*df['scale_factor']*(df['Ek']**2)*df['Ra_comp']/df['Schmidt']
+    df['Dcomp'] = df['Dcomp']*df['scale_factor']*df['Ek']/df['Schmidt']
 
 # additional parameters
-df['Dtot'] = df['Dint'] + df['Dohm'] + df['Dtemp'] + df['Dcomp']
+df['Dtot'] = df['Dint'] + df['Dohm'] + df['Dbuoy'] + df['Dbuoy_comp']
 df['Q'] = df['KE']/df['Dtot']
 
 # save results to comma-separated-value file
