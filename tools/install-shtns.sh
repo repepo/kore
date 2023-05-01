558f8a793e051247eeac42cf066336c3866e27ed
AnkitBarik <ankit.barik@gmail.com>
2023-05-01 18:36:45 -0400

    Add script for easy installation of SHTns

---
+++ tools/install-shtns.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+ver="3.5.2"
+
+if test ! -d $HOME/local; then
+    mkdir $HOME/local
+fi
+
+wget https://bitbucket.org/nschaeff/shtns/downloads/shtns-$ver.tar.gz
+tar -xvf shtns-$ver.tar.gz
+rm shtns-$ver.tar.gz
+
+if [ -d "shtns-$ver" ]
+then
+    mv shtns-$ver shtns
+fi
+
+cd shtns
+
+opts="--prefix=$HOME/.local --enable-ishioka --enable-openmp --enable-python"
+
+if [[ -n $MKLROOT ]]
+then
+   echo "MKL found, installing with MKL"
+   opts="$opts --enable-mkl"
+else
+   echo "MKL not found, will try to install with FFTW"
+fi
+
+./configure $opts
+
+if [ `echo $CC` ]
+then
+    sed -i "s/shtcc=gcc/shtcc=${CC}/" Makefile
+fi
+
+make -j
+make install -j
+
+# Python installation will fail because of sudo, install as --user
+
+python3 setup.py install --user
