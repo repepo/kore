2fefe0f361b3ff866a57c544893da6dc67a08ab4
AnkitBarik <ankit.barik@gmail.com>
2023-05-28 16:52:51 +0200

    Simple script to run Kore

---
+++ bin/runKore.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ncpus=4
+opts='-st_type sinvert -eps_error_relative ::ascii_info_detail'
+
+./submatrices.py $ncpus
+mpiexec -n $ncpus ./assemble.py
+mpiexec -n $ncpus ./solve_nopp.py $opts
+./postprocess.py
