minor bug fix in the pressure computation, added script to load solutions, including the b-field in the IC.

bin/assemble.py
@@ -161,7 +161,7 @@ def main():
                 pos3 = ut.n + ut.N1*np.where(allbot==l)[0][0]
                 row3 = np.arange(pos3+2,pos3+ut.N1)
                 col3 = np.zeros(ut.N1-2)
-                bdat3 = ut.marc_tide(ut.wf,3,par.m,'bot',ut.N1, par.ricb, ut.rcmb)*par.forcing_amplitude
+                bdat3 = ut.marc_tide(ut.wf,3,par.m,'bot',ut.N1, par.ricb, ut.rcmb)*par.forcing_amplitude_cmb
                 tmp = [bdat3, row3, col3]
                 blist = tmp
 
@@ -169,7 +169,7 @@ def main():
                 pos2 = ut.N1*np.where(alltop==l)[0][0]
                 row2 = np.arange(pos2+4,pos2+ut.N1)
                 col2 = np.zeros(ut.N1-4)
-                bdat2 = ut.marc_tide(ut.wf,2,par.m,'top',ut.N1, par.ricb, ut.rcmb)*par.forcing_amplitude
+                bdat2 = ut.marc_tide(ut.wf,2,par.m,'top',ut.N1, par.ricb, ut.rcmb)*par.forcing_amplitude_cmb
                 tmp = [bdat2, row2, col2]
                 for q in [0,1,2]:
                     blist[q]= np.concatenate((blist[q], tmp[q]))
@@ -179,7 +179,7 @@ def main():
                     pos1 = ut.n + ut.N1*np.where(allbot==l)[0][0]
                     row1 = np.arange(pos1+2,pos1+ut.N1)
                     col1 = np.zeros(ut.N1-2)
-                    bdat1 = ut.marc_tide(ut.wf,1,par.m,'bot',ut.N1, par.ricb, ut.rcmb)*par.forcing_amplitude
+                    bdat1 = ut.marc_tide(ut.wf,1,par.m,'bot',ut.N1, par.ricb, ut.rcmb)*par.forcing_amplitude_cmb
                     tmp = [bdat1, row1, col1]
                     for q in [0,1,2]:
                         blist[q]= np.concatenate((blist[q], tmp[q]))

bin/solve_nopp.py
@@ -171,21 +171,27 @@ def main():
                     ru = rEigv[ :2*ut.n, : ]
                     iu = iEigv[ :2*ut.n, : ]
 
-                # each solution for b has 2*ut.n coefs
+                # each solution for b has 2*ut.n coeffs
                 if par.magnetic == 1:
                     offset = 2*ut.n*par.hydro
                     rb = rEigv[ offset : offset + 2*ut.n, : ]
                     ib = iEigv[ offset : offset + 2*ut.n, : ]
 
+                # each solution for b_ic has 2*ut.nic coeffs
+                if ut.icflag:
+                    offset = 2*ut.n + par.magnetic * 2*ut.n
+                    rb_ic = rEigv[ offset : offset + 2*ut.nic, : ]
+                    ib_ic = iEigv[ offset : offset + 2*ut.nic, : ]
+
                 # each solution for the temperature has ut.n coeffs
                 if par.thermal == 1:
-                    offset = 2*ut.n + par.magnetic * 2*ut.n
+                    offset = 2*ut.n + par.magnetic * 2*ut.n + ut.icflag * 2*ut.nic
                     rtemp = rEigv[ offset : offset + ut.n, : ]
                     itemp = iEigv[ offset : offset + ut.n, : ]
 
                 # each solution for the composition has ut.n coeffs
                 if par.compositional == 1:
-                    offset = 2*ut.n + par.magnetic * 2*ut.n + par.thermal * ut.n
+                    offset = 2*ut.n + par.magnetic * 2*ut.n + ut.icflag * 2*ut.nic + par.thermal * ut.n
                     rcomp = rEigv[ offset : offset + ut.n, : ]
                     icomp = iEigv[ offset : offset + ut.n, : ]
 
@@ -250,13 +256,18 @@ def main():
                 rb = np.reshape(np.real(VR[ offset : offset + 2*ut.n ]),(-1,1))
                 ib = np.reshape(np.imag(VR[ offset : offset + 2*ut.n ]),(-1,1))
 
+                if ut.icflag:
+                    offset = par.hydro * 2*ut.n + par.magnetic * 2*ut.n
+                    rb_ic = np.reshape(np.real(VR[ offset : offset + 2*ut.nic ]),(-1,1))
+                    ib_ic = np.reshape(np.imag(VR[ offset : offset + 2*ut.nic ]),(-1,1))
+
             if par.thermal == 1:
-                offset = par.hydro * 2*ut.n + par.magnetic * 2*ut.n
+                offset = par.hydro * 2*ut.n + par.magnetic * 2*ut.n + ut.icflag * 2*ut.nic 
                 rtemp = np.reshape(np.real(VR[ offset : offset + ut.n ]),(-1,1))
                 itemp = np.reshape(np.imag(VR[ offset : offset + ut.n ]),(-1,1))
 
             if par.compositional == 1:
-                offset = par.hydro * 2*ut.n + par.magnetic * 2*ut.n + par.thermal * ut.n
+                offset = par.hydro * 2*ut.n + par.magnetic * 2*ut.n + ut.icflag * 2*ut.nic + par.thermal * ut.n
                 rcomp = np.reshape(np.real(VR[ offset : offset + ut.n ]),(-1,1))
                 icomp = np.reshape(np.imag(VR[ offset : offset + ut.n ]),(-1,1))
 
@@ -281,25 +292,31 @@ def main():
                     np.savetxt(deig, eigval)
 
             # one solution per column
-            if par.hydro == 1:
+            if par.hydro:
                 with open('real_flow.field','wb') as dflo1:
                     np.savetxt(dflo1, ru)
                 with open('imag_flow.field','wb') as dflo2:
                     np.savetxt(dflo2, iu)
 
-            if par.magnetic == 1:
+            if par.magnetic:
                 with open('real_magnetic.field','wb') as dmag1:
                     np.savetxt(dmag1, rb)
                 with open('imag_magnetic.field','wb') as dmag2:
                     np.savetxt(dmag2, ib)
 
-            if par.thermal == 1:
+            if ut.icflag:
+                with open('real_magnetic_ic.field','wb') as dmag1_ic:
+                    np.savetxt(dmag1_ic, rb_ic)
+                with open('imag_magnetic_ic.field','wb') as dmag2_ic:
+                    np.savetxt(dmag2_ic, ib_ic)
+
+            if par.thermal:
                 with open('real_temperature.field','wb') as dtemp1:
                     np.savetxt(dtemp1, rtemp)
                 with open('imag_temperature.field','wb') as dtemp2:
                     np.savetxt(dtemp2, itemp)
 
-            if par.compositional == 1:
+            if par.compositional:
                 with open('real_composition.field','wb') as dcomp1:
                     np.savetxt(dcomp1, rcomp)
                 with open('imag_composition.field','wb') as dcomp2:

bin/spin_doctor.py
@@ -166,7 +166,7 @@ def main(ncpus):
             vtorq[i] = par.Ek * np.dot( ut.gamma_visc(0,0,0), u_sol)  # need to double check the constants here
             vtorq_icb[i] = par.Ek * np.dot( ut.gamma_visc_icb(par.ricb), u_sol)
 
-            press0 = udgn[6][0] 
+            press0[i] = udgn[6][0] 
 
 
         if par.magnetic:
@@ -211,7 +211,7 @@ def main(ncpus):
         # resid3 is the relative residual of 2*sigma*TE - Dthm - Wadv_thm = 0
         # ---------------------------------------------------------------------------------------------------------
 
-        repow = 0  # power from the forcing needs to be computed, not coded yet. 
+        [repow, pss, pvf] = [0, 0, 0]  # power from the forcing needs to be computed, not coded yet. 
 
         if par.forcing == 0:
             pss = 0

bin/utils.py
@@ -35,9 +35,12 @@ vsymm  = par.symm
 if ((par.innercore == 'conducting, Chebys') or ('perfect' in par.innercore)) and par.magnetic and par.ricb>0:
     secf_projection = 'consoidal'  # Consoidal projection in section f, basis C^(3)
     cic = 1
+    if par.innercore == 'conducting, Chebys':
+        icflag = 1
 else:
     secf_projection = 'radial'     # Radial projection in section f, basis C^(2)
     cic = 0
+    icflag = 0
 
 symm1 = (2*np.sign(par.m) - 1) * par.symm  # symm1=par.symm if m>0, symm1 = -par.symm if m=0
 
@@ -1296,13 +1299,13 @@ def gamma_magnetic():
         if B0_l == 1:  # Either uniform axial or dipole background field, induced magnetic field b is thus antisymmetric
 
             # the torque is prop. to the l=2 toroidal component of b
-            out[0,n0:n0+par.N] = (16*np.pi/5) * G * h_cmb
+            out[0,n0:n0+par.N] = -(16*np.pi/5) * G * h_cmb
 
         elif B0_l == 2:  # Quadrupole background field, induced magnetic field b is thus symmetric
 
             # torque prop. to l=1 and l=3 toroidal component of b
-            out[0,n0:n0+par.N]          = -(16*np.pi/5)     * G * h_cmb # l=1
-            out[0,n0+par.N: n0+2*par.N] =  (16*18*np.pi/35) * G * h_cmb # l=3
+            out[0,n0:n0+par.N]          =  (16*np.pi/5)     * G * h_cmb # l=1
+            out[0,n0+par.N: n0+2*par.N] = -(16*18*np.pi/35) * G * h_cmb # l=3
 
     else:
 
@@ -1314,3 +1317,34 @@ def gamma_magnetic():
 
     return out
 
+
+
+def gamma_magnetic_ic():
+    '''
+    Axial magnetic torque on the inner core (spherical). Needs m=0 and symm=1. 
+    '''
+
+    if (par.magnetic==1 and par.m == 0 and par.symm==1 and ( ('conducting' in par.innercore) or ('TWA' in par.innercore) ) ):
+
+        out = np.zeros((1,n0+n0),dtype=complex)
+        G = Tk( -1, par.N-1, 0)[:,0]
+        ric = np.array([par.ricb])
+        h_icb = B0_norm() * h0(ric, par.B0, [par.beta, par.B0_l, par.ricb, 0])
+
+        if B0_l == 1:  # Either uniform axial or dipole background field, induced magnetic field b is thus antisymmetric
+
+            # the torque is prop. to the l=2 toroidal component of b
+            out[0,n0:n0+par.N] = (16*np.pi/5) * G * h_icb
+
+        elif B0_l == 2:  # Quadrupole background field, induced magnetic field b is thus symmetric
+
+            # torque prop. to l=1 and l=3 toroidal component of b
+            out[0,n0:n0+par.N]          = -(16*np.pi/5)     * G * h_icb # l=1
+            out[0,n0+par.N: n0+2*par.N] =  (16*18*np.pi/35) * G * h_icb # l=3
+
+    else:
+
+        out = 0
+
+
+    return out