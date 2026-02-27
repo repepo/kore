12cba52bf63df3dfbbed64c10f4bf5d076e8b4ef
daoudlaariara <daoudl@outlook.com>
2026-02-27 15:42:14 +0100

    Changed laod_env.sh

--- tools/load_env.sh
+++ tools/load_env.sh
@@ -6,15 +6,16 @@ export PYENV_ROOT="$HOME/.pyenv"
 eval "$(pyenv init - bash)"
 
 # Load petsc, slepc
-export PETSC_DIR=/home/ucl/mema/dlaariar/.local/src/kore/petsc-3.12.5
+export PETSC_DIR=/home/ucl/mema/dlaariar/.local/src/kore/petsc
 export PETSC_ARCH=arch-linux-c-opt
 export PATH=$PETSC_DIR/$PETSC_ARCH/bin:$PATH
-export SLEPC_DIR=/home/ucl/mema/dlaariar/.local/src/kore/slepc-3.12.2
+export SLEPC_DIR=/home/ucl/mema/dlaariar/.local/src/kore/slepc
 export PYTHONPATH=$PETSC_DIR/$PETSC_ARCH/lib
 
 # Load modules
-module purge
+# module purge
 module load foss
 
 # Kore directory
-export KORE_HOME="/home/ucl/mema/dlaariar/.local/src/kore/kore"
+#export KORE_HOME="/home/ucl/mema/dlaariar/.local/src/kore/kore"
+export KORE_HOME="/home/ucl/mema/dlaariar/.local/src/kore/polytropic_dr/kore"
